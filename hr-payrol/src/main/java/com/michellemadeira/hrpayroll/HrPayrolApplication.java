04b542cd9f944e5c1500c68dc335ca4814059107
Michelle Madeira <michellemadeira1@gmail.com>
2023-02-20 13:56:15 -0300

    Project hr-payroll created

---
+++ hr-payrol/src/main/java/com/michellemadeira/hrpayroll/HrPayrolApplication.java
@@ -0,0 +1,13 @@
+package com.michellemadeira.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrolApplication.class, args);
+	}
+
+}
---
+++ hr-payrol/src/test/java/com/michellemadeira/hrpayroll/HrPayrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michellemadeira.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
