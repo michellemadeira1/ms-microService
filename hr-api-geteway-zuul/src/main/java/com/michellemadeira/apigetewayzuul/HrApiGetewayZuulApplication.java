a399bd5ffbd38089e272a69c2dd8ae9020f18b4a
Michelle Madeira <michellemadeira1@gmail.com>
2023-02-25 10:14:52 -0300

    Project hr-api-geteway-zuul created

---
+++ hr-api-geteway-zuul/src/main/java/com/michellemadeira/apigetewayzuul/HrApiGetewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.michellemadeira.apigetewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGetewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGetewayZuulApplication.class, args);
+	}
+
+}
